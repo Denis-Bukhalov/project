ae11da2a4a35a475cc3eda32dba95bf961b8a041
Denis Bukhalov <buhalovdenisomsk@gmail.com>
2022-12-11 22:06:16 +0600

    keycloak!

--- src/main/java/whim/project/ProjectApplication.java
+++ src/main/java/whim/project/ProjectApplication.java
@@ -3,7 +3,13 @@ package whim.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.Authentication;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
@@ -27,6 +33,24 @@ public class ProjectApplication {
 	@GetMapping("/")
 	public String home() {
 		return "index";
+	}
+
+	@GetMapping("/student")
+	@PreAuthorize("hasRole('STUDENT')")
+	public String student() {
+		return "STUDENT REEEEEEEEEEEEEE";
+	}
+
+	@GetMapping("/lecturer")
+	@PreAuthorize("hasRole('LECTURER')")
+	public String lecturer() {
+		return "LECTURER REEEEEEEEEEEEEE";
+	}
+
+	@GetMapping("/me")
+	public Object getMe() {
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication.getName();
 	}
 
 }
